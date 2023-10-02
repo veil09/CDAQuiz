9be8f5e355f6b12ec0ef84e2c75b52da1bc9e30f
Aaron Herezano <73149579+veil09@users.noreply.github.com>
2023-10-02 11:23:26 +0800

    CDA updated

--- main.js
+++ main.js
@@ -1,42 +1,22 @@
-const imageFemale = document.querySelector(".female");
-const fadeInFemale = setInterval(() => {
-  imageFemale.style.opacity = "1";
-}, 100);
+function animateElement(element, property, value, duration) {
+  const targetElement = document.querySelector(element);
+  const animationInterval = setInterval(() => {
+    targetElement.style[property] = value;
+  }, duration);
 
-const headline_one = document.querySelector(".headline_1");
-const headline_1 = setInterval(() => {
-  headline_one.style.left = "0";
-}, 1000);
+  // Clear the interval after the animation duration
+  setTimeout(() => {
+    clearInterval(animationInterval);
+  }, duration);
+}
 
-const hideFirstEffect = setTimeout(() => {
-  imageFemale.style.display = "none";
-}, 3000);
-
-const hideHealineOne = setTimeout(() => {
-  headline_one.style.display = "none";
-}, 5000);
-
-const headline_two = document.querySelector(".headline_2");
-const headline_2 = setInterval(() => {
-  headline_two.style.right = "0";
-}, 4000);
-
-const subHeadline_one = document.querySelector(".subHeadline");
-const subHeadline_1 = setInterval(() => {
-  subHeadline_one.style.opacity = "1";
-}, 5000);
-
-const learnmore = document.querySelector(".learnmore");
-const learnmore_1 = setInterval(() => {
-  learnmore.style.opacity = 1;
-}, 5500);
-
-const logos = document.querySelector(".logo");
-const logo = setInterval(() => {
-  logos.style.left = "15rem";
-}, 6000);
-
-const replayImg = document.querySelector(".replay");
-const replay = setInterval(() => {
-  replayImg.style.opacity = "1";
-}, 6500);
+// Define the animations
+animateElement(".female", "opacity", "1", 100);
+animateElement(".headline_1", "left", "0", 1000);
+animateElement(".female", "display", "none", 3000);
+animateElement(".headline_1", "display", "none", 5000);
+animateElement(".headline_2", "right", "0", 4000);
+animateElement(".subHeadline", "opacity", "1", 5000);
+animateElement(".learnmore", "opacity", "1", 5500);
+animateElement(".logo", "left", "15rem", 6000);
+animateElement(".replay", "opacity", "1", 6500);
